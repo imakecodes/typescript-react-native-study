1d87e02de7eb73bf67126c6c3cd8cc887f92a0b1
Michel Wilhelm <michelwilhelm@gmail.com>
2020-12-17 01:05:06 -0300

    Listando os itens no carrinho de compras

--- src/components/CartItem/index.tsx
+++ src/components/CartItem/index.tsx
@@ -4,18 +4,26 @@ import { View, Text, TouchableOpacity } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 
 import styles from "./styles";
+import CartService from "../../services/cart";
 
-function CartItemComponent() {
+
+const CartItemComponent = (props: any) => {
+  const { item, refreshCart } = props;
+  const handleRemoveItem = () => {
+    CartService.deleteByProductId(item.product_id);
+    refreshCart()
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.itemDescriptionContainer}>
-        <Text style={styles.itemName}>HAAR INTERN C/60CPR-S/AC</Text>
+        <Text style={styles.itemName}>{item.product.name}</Text>
 
         <View style={styles.itemPriceContainer}>
-          <Text style={styles.itemQuantity}>100un</Text>
-          <Text style={styles.itemValue}>R$ 180.00</Text>
+          <Text style={styles.itemQuantity}>{`${item.quantity}un`}</Text>
+          <Text style={styles.itemValue}>R$ {(item.product.price_final * item.quantity).toFixed(2)}</Text>
 
-          <TouchableOpacity style={styles.removeItemButton}>
+          <TouchableOpacity style={styles.removeItemButton} onPress={handleRemoveItem}>
             <MaterialIcons name="delete-forever" size={22} color="#f62854" />
           </TouchableOpacity>
         </View>
--- src/pages/ProductsPage/index.tsx
+++ src/pages/ProductsPage/index.tsx
@@ -38,9 +38,9 @@ function ProductsPage() {
           paddingBottom: 16,
         }}
       >
-        {products.map((product: Product) => {
-          return <ProductCardComponent key={product.id} {...product} />;
-        })}
+        {products.map((product: Product) => (
+          <ProductCardComponent key={product.id} {...product} />
+        ))}
       </ScrollView>
     </View>
   );
